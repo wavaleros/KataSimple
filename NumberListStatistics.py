e2b470cef21306b93456c5010aefea25692afe94
wavaleros <wavaleros@gmail.com>
2020-03-16 22:37:48 -0500

    Azul - WV - Refactor casting de variable

--- NumberListStatistics.py
+++ NumberListStatistics.py
@@ -1,21 +1,23 @@
 class NumberListStatistics:
-    
+
     def __init__(self):
         pass
-    
-    def processString(self, input_list): 
+
+    def processString(self, input_list):
         length = 0
-        minimo = -1
-        result = [length, minimo]
+        minimum = -1
+        result = [length, minimum]
         for i in input_list.split(','):
             if i != '':
-                length = length+1
-                if minimo == -1 or int(i) < minimo:
-                    minimo = int(i)
+                j = int(i)
+                length = length + 1
+                if minimum == -1 or j < minimum:
+                    minimum = j
 
         result[0] = length
-        result[1] = int(minimo)
+        result[1] = minimum
         return result
+
 
 if __name__ == '__main__':
     print()
