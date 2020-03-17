ab650b62971104bddbaa131a3381f45a9e6c84a5
Ing Juan Emilio Galindo <juanemilio138@gmail.com>
2020-03-16 21:14:02 -0500

    Rojo Lista con un elemento

--- test_NumberListStatistics.py
+++ test_NumberListStatistics.py
@@ -11,3 +11,8 @@ class test_number_list_size(TestNumberListStatistics):
 
     def test_empty_list_return_size_zero(self):
         self.assertTrue(self.testedClass.get_list_size() == 0)
+
+
+    def test_list_return_size_one(self):
+        self.testedClass.processString("1")
+        self.assertTrue(self.testedClass.get_list_size() == 0)
