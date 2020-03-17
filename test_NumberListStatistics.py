51e594a168f508226722cd4371ede20a7536d5fd
wavaleros <wavaleros@gmail.com>
2020-03-16 21:59:28 -0500

    Rojo - WV - Minimo para lista vacia

--- test_NumberListStatistics.py
+++ test_NumberListStatistics.py
@@ -28,3 +28,9 @@ class test_number_list_size(TestNumberListStatistics):
         input_list = "1,2,6,7,8,341234,1234"
         self.testedClass.processString(input_list)
         self.assertTrue(self.testedClass.list_legth == len(input_list.split(',')))
+
+
+class test_minimum_value(TestNumberListStatistics):
+    def test_return_minus_one_with_no_input(self):
+        response = self.testedClass.processString("")
+        self.assertEqual(-1, response[2])
