69f242c582b1452bce002bfa18a9dd9878598fe1
wavaleros <wavaleros@gmail.com>
2020-03-16 20:23:35 -0500

    [WV]Commit Inicial

---
+++ NumberListStatistics.py
@@ -0,0 +1,7 @@
+class NumberListStatistics:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    print()
---
+++ test_NumberListStatistics.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+from NumberListStatistics import NumberListStatistics
+
+
+class TestNumberListStatistics(TestCase):
+    testedClass = NumberListStatistics()
