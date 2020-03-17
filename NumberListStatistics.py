c8cf0ba6cb4c30b22ed49726c99ae067cb46dd3a
wavaleros <wavaleros@gmail.com>
2020-03-16 22:50:23 -0500

    Verde WV  maximo con un elemento

--- NumberListStatistics.py
+++ NumberListStatistics.py
@@ -14,6 +14,7 @@ class NumberListStatistics:
                 length = length + 1
                 if minimum == -1 or j < minimum:
                     minimum = j
+                maximum = j
 
         result[0] = length
         result[1] = minimum
