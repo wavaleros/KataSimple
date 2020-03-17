2deb39ed6382e8146bda109f0ec2ffcdeae41b58
wavaleros <wavaleros@gmail.com>
2020-03-16 20:52:02 -0500

    WV-Rojo-Lista contiene 0 elementos

--- test_NumberListStatistics.py
+++ test_NumberListStatistics.py
@@ -5,3 +5,9 @@ from NumberListStatistics import NumberListStatistics
 
 class TestNumberListStatistics(TestCase):
     testedClass = NumberListStatistics()
+
+
+class test_number_list_size(TestNumberListStatistics):
+
+    def test_empty_list_return_size_zero(self):
+        self.assertTrue(self.testedClass.get_list_size())
